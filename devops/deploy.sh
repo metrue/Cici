0bf86130715a71f838a4af39e3b636302c1beb90
metrue <h.minghe@gmail.com>
2017-02-09 18:54:54 +0800

    try to deploy to host

---
+++ devops/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env=$1
+
+host='root@106.185.44.133'
+ssh ${host} <<END
+  docker rmi \$(docker images --filter "dangling=true" -q --no-trunc)
+  rm -rf /tmp/Sira
+  git clone git@github.com:asmalltalk/asmalltalk.github.io.git /tmp/asmalltalk.com
+  cd /tmp/asmalltalk.com
+  docker build -f devops/dockerfile.nginx -t seal .
+  docker run -p 80:80 seal
+END

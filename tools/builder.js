7b938893c8f4475e291ef4def03bd4c531de3ce1
metrue <h.minghe@gmail.com>
2017-02-19 10:04:41 +0800

    clean up

--- tools/builder.js
+++ tools/builder.js
@@ -10,7 +10,6 @@ import HomePage from '../lib/js/HomePage.build.js'
 import HOME_TEMPLATE from './home.tpl'
 import PAGE_TEMPLATE from './page.tpl'
 
-
 const DEFAULT_INPUT_DIR = './posts'
 const DEFAULT_OUTPUT_DIR = './public'
 
@@ -18,36 +17,6 @@ const BUILD_DONE = 'BUILD_DONE'
 
 const ABS_PATH_REGEX = /^\//
 const PWD = process.cwd()
-
-const TEMPLATE = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,user-scalable=no">
-  <meta name="renderer" content="webkit">
-  <meta name="theme-color" content="#ffffff">
-  <link href="/css/build.HomePage.css" rel="stylesheet">
-  <link href="/css/build.PostPage.css" rel="stylesheet">
-  <title> {{ title }}</title>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', '<%= ga %>', 'auto');
-    ga('send', 'pageview');
-  </script>
-</head>
-<body>
-  <%- pageBody %>
-  <footer class="footer">
-    Copyright ©2014-2017 <a href="<%= website %>"> <%= domain %></a> | Powered by <a href="https://github.com/metrue/Seal">Seal</a> on top of <a href="https://vuejs.org" target="_blank">Vue.js</a>
-  </footer>
-<body>
-</html>
-`
 
 export default class Builder {
   constructor(config = {}) {
